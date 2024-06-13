b528a0958d1ad86de2cc5a4d1bc4d6741656fc5c
Shiv Prajapati <prajapatishiv1006@gmail.com>
2024-06-13 11:43:31 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
     if (window.scrollY > 50) {
-        navbar.style.backgroundColor = '#093145';
-    } else {
         navbar.style.backgroundColor = '#0a84ee';
+    } else {
+        navbar.style.backgroundColor = '#093145';
     }
 });
 
